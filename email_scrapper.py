a4a356460b0beb3dcb07875a0267001f86ac684d
Fawaz <faa5019@rit.edu>
2018-02-05 20:22:48 -0500

    complete version 1 of the scrapper, functional state

---
+++ email_scrapper.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+at_replacables = ["[at]","-at-", "(at)", " at ",  " @ ", "@ ", " @"]
+dot_replacables = ["[dot]", "-dot-", "(dot)", " dot ", " . ", " .", ". "] 
+regex = "([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)"
+
+def replaceObscure(txt):
+  txt = txt.lower()
+  for replacable in at_replacables:
+    txt = txt.replace(replacable, "@")
+  for replacable in dot_replacables:
+    txt = txt.replace(replacable, ".")
+  return txt
+
+#line = "test@hotmail.com asldkfja sdf;lkasjdf asldkf asdkfj asdflkj sdkfj 2sdfsdf@asdfsdf.com fawazz at kskom   zgat[at]xxx.com asdf(at)asdf(dot)com x @ testing . com"
+emails = []
+quote_page = None
+while type(quote_page) != str:
+  quote_page = input("enter page to scrape:")
+
+page = requests.get(quote_page)
+soup = BeautifulSoup(page.content,"html5lib")
+#for line in soup.get_text().splitlines():
+txt = replaceObscure(soup.get_text())
+#print(txt)
+foundEmails = re.findall(regex,txt)  
+print(foundEmails)
